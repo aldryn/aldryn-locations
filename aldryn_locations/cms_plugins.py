aldryn/aldryn-locations

commit f76dedd7600178e49293b4dc08af354e348326be

path location plugin: fix absolute url

aldryn_locations/cms_plugins.py
@@ -50,7 +50,7 @@ class MapCMSPlugin(LocationsBase):
 
         for child in instance.child_plugin_instances or []:
             data = child.get_location_data_for_map()
-            if isinstance(child, models.PathLocationPlugin):
+            if not data.startswith('http') and isinstance(child, models.PathLocationPlugin):
                 # make url absolute (required by Google API)
                 data = '{}{}'.format(base_url, data)
                 path_sources.append(data)

commit 0f004f45d94d62fb182baa514cfbf230c39e7449

fix error introduced in f76dedd7

aldryn_locations/cms_plugins.py
@@ -50,9 +50,10 @@ class MapCMSPlugin(LocationsBase):
 
         for child in instance.child_plugin_instances or []:
             data = child.get_location_data_for_map()
-            if not data.startswith('http') and isinstance(child, models.PathLocationPlugin):
-                # make url absolute (required by Google API)
-                data = '{}{}'.format(base_url, data)
+            if isinstance(child, models.PathLocationPlugin):
+                if not data.startswith('http'):
+                    # make url absolute (required by Google API)
+                    data = '{}{}'.format(base_url, data)
                 path_sources.append(data)
             else:
                 location_data.append(data)
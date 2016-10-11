0f004f45d94d62fb182baa514cfbf230c39e7449
Kim Thoenen <kim@smuzey.ch>
2016-10-11 13:35:16 +0200

    fix error introduced in f76dedd7

--- aldryn_locations/cms_plugins.py
+++ aldryn_locations/cms_plugins.py
@@ -50,9 +50,10 @@ class MapCMSPlugin(LocationsBase):
 
         for child in instance.child_plugin_instances or []:
             data = child.get_location_data_for_map()
-            if not data.startswith('http') and isinstance(child, models.PathLocationPlugin):
-                # make url absolute (required by Google API)
-                data = '{}{}'.format(base_url, data)
+            if isinstance(child, models.PathLocationPlugin):
+                if not data.startswith('http'):
+                    # make url absolute (required by Google API)
+                    data = '{}{}'.format(base_url, data)
                 path_sources.append(data)
             else:
                 location_data.append(data)
